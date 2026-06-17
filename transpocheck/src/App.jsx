4395a243aaf6bc53f008d05ed43366d38372b221
Felipe <fantoniocastronunez@gmail.com>
2026-06-17 07:57:55 -0400

    1.9.2 2

--- transpocheck/src/App.jsx
+++ transpocheck/src/App.jsx
@@ -1891,7 +1891,7 @@ export default function App() {
                 </div>
                 {/* VERSIÓN DE LA APP */}
                 <div className="bg-slate-50 p-2.5 text-center border-t border-slate-100">
-                  <p className="text-[10px] font-black text-slate-400 tracking-widest uppercase">LogisticAPP v.1.9.2/p>
+                  <p className="text-[10px] font-black text-slate-400 tracking-widest uppercase">LogisticAPP v.1.9.2</p>
                 </div>
               </div>
             )}
